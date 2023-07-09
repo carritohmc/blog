5081efd1354b24120a8ea210d4eadd963b1cd000
Felix Alm <carritohmc@gmail.com>
2023-07-09 09:27:07 -0400

    Blog project class 4

---
+++ posts/views.py
@@ -0,0 +1,48 @@
+from django.views.generic import (
+    ListView, 
+    DetailView, 
+    CreateView,
+    UpdateView,
+    DeleteView
+)
+from django.contrib.auth.mixins import (
+    LoginRequiredMixin,
+    UserPassesTestMixin
+)
+from django.urls import reverse_lazy
+from .models import Post
+
+class PostListView(ListView):
+    template_name = "posts/list.html"
+    model = Post
+
+class PostDetailView(DetailView):
+    template_name = "posts/detail.html"
+    model = Post
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    template_name = "posts/new.html"
+    model = Post
+    fields = ["title", "subtitle", "body"]
+
+    def form_valid(self, form):
+        form.instance.author =  self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    template_name ="posts/edit.html"
+    model = Post
+    fields = ["title", "subtitle", "body"]
+
+    def test_func(self):
+        post_obj = self.get_object()
+        return post_obj.author == self.request.user
+
+class PostDeleteView (LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    template_name = "posts/delete.html"
+    model = Post
+    success_url = reverse_lazy("list")
+
+    def test_func(self):
+        post_obj = self.get_object()
+        return post_obj.author == self.request.user
